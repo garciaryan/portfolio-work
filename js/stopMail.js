c17fef090693590963fdd143b393cd9d32139984
Ryan Garcia <garciaryan007@yahoo.com>
2017-07-06 16:29:01 -0700

    removed logs

--- js/stopMail.js
+++ js/stopMail.js
@@ -2,9 +2,7 @@ $(':input[type=submit]').prop('disabled', true);
 
 $(function (){
   $('.input').keyup(function (){
-    console.log('Im all keyed up');
     if ($('.input').val().length >= 1){
-      console.log($('.input').val().length);
       $(':input[type=submit]').prop('disabled', false);
     }
     else {
