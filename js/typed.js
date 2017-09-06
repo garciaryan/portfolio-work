be7962f2c14bfcf942c38bada558c7411bfce40b
Ryan Garcia <garciaryan007@yahoo.com>
2017-09-05 17:18:32 -0700

    typejs added

---
+++ js/typed.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.typed').typed({
+        strings: ['my name is ryan.', 'i build responsive websites.'],
+        typeSpeed: 80,
+        showCursor: true,
+        backSpeed: 40,
+        backDelay: 3500,
+    });
+});
