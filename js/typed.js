5ed2e614eda157e6ec3fa959c299d5d222b59eac
Ryan Garcia <garciaryan007@yahoo.com>
2017-11-03 16:48:40 -0700

    edits title and header padding

--- js/typed.js
+++ js/typed.js
@@ -1,8 +1,8 @@
 $(function() {
     $('.typed').typed({
         strings: ["my name is ryan", "i build responsive websites", "i build web applications", "i build mobile applications"],
-        typeSpeed: 40,
         smartBackspace: true,
+        typeSpeed: 40,
         showCursor: true,
         backSpeed: 20,
         backDelay: 3000,
