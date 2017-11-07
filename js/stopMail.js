0fc584addef5db6f38d429e5d58280e11479eb3a
Ryan Garcia <garciaryan007@yahoo.com>
2017-11-06 16:30:26 -0800

    undo comments

--- js/stopMail.js
+++ js/stopMail.js
@@ -1,15 +1,15 @@
 // Prevents user from submitting empty form.
-// $(':input[type=submit]').attr('disabled', true);
-//
-// $(function (){
-//   $('.input').keyup(function (){
-//     if ($('.input').val().length >= 1){
-//       $(':input[type=submit]').attr('disabled', false);
-//     }
-//     else {
-//       $(':input[type=submit]').attr('disabled', true);
-//     }
-//   });
-// });
+$(':input[type=submit]').attr('disabled', true);
+
+$(function (){
+  $('.input').keyup(function (){
+    if ($('.input').val().length >= 1){
+      $(':input[type=submit]').attr('disabled', false);
+    }
+    else {
+      $(':input[type=submit]').attr('disabled', true);
+    }
+  });
+});
 
 emailjs.sendForm('mailgun', 'template_upOEUwrE', 'contact_form');
