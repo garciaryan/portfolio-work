0346da98ea6fcbd6353543925fda00fb9063819a
Ryan Garcia <garciaryan007@yahoo.com>
2017-11-06 16:30:01 -0800

    redoing mail functionality with captcha

--- js/stopMail.js
+++ js/stopMail.js
@@ -1,12 +1,15 @@
-$(':input[type=submit]').attr('disabled', true);
+// Prevents user from submitting empty form.
+// $(':input[type=submit]').attr('disabled', true);
+//
+// $(function (){
+//   $('.input').keyup(function (){
+//     if ($('.input').val().length >= 1){
+//       $(':input[type=submit]').attr('disabled', false);
+//     }
+//     else {
+//       $(':input[type=submit]').attr('disabled', true);
+//     }
+//   });
+// });
 
-$(function (){
-  $('.input').keyup(function (){
-    if ($('.input').val().length >= 1){
-      $(':input[type=submit]').attr('disabled', false);
-    }
-    else {
-      $(':input[type=submit]').attr('disabled', true);
-    }
-  });
-});
+emailjs.sendForm('mailgun', 'template_upOEUwrE', 'contact_form');
